aff34cb1762db04fd60fdac2dfd0ef3fef787bcd
PratsN <pratikshanagawade0@gmail.com>
2023-03-16 19:09:53 +0530

    links

--- src/Components/CardData.js
+++ src/Components/CardData.js
@@ -1,61 +1,60 @@
-import React  from "react";
-import {
-    Card,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-  } from "@mui/material";
-import "./CardData.css"
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import React from "react";
+import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
+import "./CardData.css";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
 
+const CardData = ({ video }) => {
+  const { title, image, date, category, link } = video;
 
-const CardData = ({video}) => {
-
-const {title , image , date , category} = video;
-
-
-
-    return (
-     <>
-       <Card >
-            <CardActionArea 
-             sx={{'&:hover': {
-              backgroundColor:"#616161",
-              opacity: ".9",         
-          }}}
-          
+  return (
+    <>
+      <Card>
+        <CardActionArea
+          sx={{
+            "&:hover": {
+              backgroundColor: "#616161",
+              opacity: ".9",
+            },
+          }}
+        >
+          <a
+            target="_blank"
+            href={link}
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
           >
-            <CardMedia
-            component="img"
-            height="200"
-            image={image}
-            alt={title} />
+            <CardMedia component="img" height="200" image={image} alt={title} />
 
-            <CardContent style={{backgroundColor: "black" , color: "white" , height : "20vh"}} >
-            <div className="text-style">
-            {title}       
-            </div>
-            <div className="card-content">
+            <CardContent
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                height: "20vh",
+              }}
+            >
+              <div className="text-style">{title}</div>
+              <div className="card-content">
                 <div className="sub-content">
-                    
-                    <div>{category}</div>
-                    <div>{date}</div>
-
+                  <div>{category}</div>
+                  <div>{date}</div>
                 </div>
 
-                <div >
-                <Checkbox sx={{color : "white"}} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                <div>
+                  <Checkbox
+                    sx={{ color: "white" }}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                  />
                 </div>
-            </div>
-             
+              </div>
             </CardContent>
-            </CardActionArea>
-        </Card>
-        </>
-
-    )
-}
+          </a>
+        </CardActionArea>
+      </Card>
+    </>
+  );
+};
 
 export default CardData;
